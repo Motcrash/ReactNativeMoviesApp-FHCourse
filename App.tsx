72673c0b9703d69fad192cab32955c11b911dd7f
Motcrash <Motcrash@gmail.com>
2023-07-10 22:31:26 -0600

    test

--- App.tsx
+++ App.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNav } from './src/navigation/Navigation';
+import { FadeScreen } from './src/screens/FadeScreen';
 const App = () => {
   return (
     <NavigationContainer>
       <StackNav/> 
+
+      {/* <FadeScreen/>  */}
     </NavigationContainer>
   )
 }
